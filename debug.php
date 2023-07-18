29abf7dec934466974316b7818377acc744de721
Wijaya Rakhmat <wijaya.rakhmat@hotmail.com>
2023-07-18 02:30:44 +0000

    Content download

--- debug.php
+++ debug.php
@@ -2,6 +2,14 @@
 
 function main() {
 	echo '<p>Hello, world!</p>';
+
+	$urlf = 'https://sirup.lkpp.go.id/sirup/home/detailPaketPenyediaPublic2017/%d';
+	$id = 38401264;
+
+	$html = file_get_contents(sprintf($urlf, $id));
+
+	printf('<p>%s</p>', sprintf($urlf, $id));
+	printf('<div style="%s">%s</div>', 'white-space: pre-wrap;', htmlspecialchars($html));
 }
 
 main();
