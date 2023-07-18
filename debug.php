30cdbcc4a7b8faa57a459ef73e3984d6beb74b48
Wijaya Rakhmat <wijaya.rakhmat@hotmail.com>
2023-07-18 03:46:43 +0000

    Nonexistent content

--- debug.php
+++ debug.php
@@ -2,7 +2,13 @@
 
 function main() {
 	echo '<p>Hello, world!</p>';
+	// debug1();
+	// debug2();
+	// debug3();
+}
 
+/* Content download */
+function debug1() {
 	$urlf = 'https://sirup.lkpp.go.id/sirup/home/detailPaketPenyediaPublic2017/%d';
 	$id = 38401264;
 
@@ -12,7 +18,10 @@ function main() {
 	printf('<summary>%s</summary>', sprintf($urlf, $id));
 	printf('<div style="%s">%s</div>', 'white-space: pre-wrap;', htmlspecialchars($html));
 	echo '</details>';
+}
 
+/* Flush buffer */
+function debug2() {
 	for ($i = 0; $i < 10; ++$i) {
 		printf('<p>%d</p>', $i + 1);
 		ob_flush();
@@ -20,6 +29,17 @@ function main() {
 		sleep(1);
 	}
 	echo '<p>Done</p>';
+}
+
+/* Nonexistent content */
+function debug3() {
+	$url = 'https://sirup.lkpp.go.id/sirup/home/detailPaketPenyediaPublic2017/99999999';
+	printf('<p>%s</p>', $url);
+	$html = @file_get_contents($url);
+	if ($html)
+		printf('<div style="%s">%s</div>', 'white-space: pre-wrap;', htmlspecialchars($html));
+	else
+		echo '<p>Content does not exists.</p>';
 }
 
 main();
