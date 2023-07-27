89cc2d95ea8c09492a42bc908d6fc8e63d496b06
Wijaya Rakhmat <wijaya.rakhmat@hotmail.com>
2023-07-27 03:01:28 +0000

    Upload schema

---
+++ schema/raw.sql
@@ -0,0 +1,57 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 15.3
+-- Dumped by pg_dump version 15.3
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: raw; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.raw (
+    auto_id integer NOT NULL,
+    "timestamp" timestamp without time zone DEFAULT now() NOT NULL,
+    website text NOT NULL,
+    type text,
+    id integer NOT NULL,
+    content text NOT NULL
+);
+
+
+--
+-- Name: raw_auto_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+ALTER TABLE public.raw ALTER COLUMN auto_id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.raw_auto_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- Name: raw raw_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.raw
+    ADD CONSTRAINT raw_pkey PRIMARY KEY (auto_id);
+
+
+--
+-- PostgreSQL database dump complete
+--
