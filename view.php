097eb7820ebf0559854fb33979c6db4064f3d4df
Wijaya Rakhmat <wijaya.rakhmat@hotmail.com>
2023-07-28 06:34:31 +0000

    Rework view
    Fix padding
    Fix enter
    Show bounds

--- view.php
+++ view.php
@@ -11,9 +11,6 @@ if (!isset($data['website']) || strlen($data['website']) == 0) {
 if (!isset($data['type']) || strlen($data['type']) == 0)
 	$data['type'] = null;
 
-if (array_key_exists('jump', $data))
-	$data['id'] = $data['jump'];
-
 if (!isset($data['id']) || strlen($data['id']) == 0)
 	switch ([$data['website'], $data['type']]) {
 	case ['sirup', 'satuan']:
@@ -35,51 +32,30 @@ if (!isset($data['id']) || strlen($data['id']) == 0)
 else
 	$data['id'] = (int) $data['id'];
 
-?>
+switch ([$data['website'], $data['type']]) {
+case ['sirup', 'satuan']:
+	$data['urlf'] = 'https://sirup.lkpp.go.id/sirup/home/showPaKpaModal?idSatker=%d';
+	break;
+case ['sirup', 'penyedia']:
+	$data['urlf'] = 'https://sirup.lkpp.go.id/sirup/home/detailPaketPenyediaPublic2017/%d';
+	break;
+case ['sirup', 'swakelola']:
+	$data['urlf'] = 'https://sirup.lkpp.go.id/sirup/home/detailPaketSwakelolaPublic2017?idPaket=%d';
+	break;
+case ['modi', null]:
+	$data['urlf'] = 'https://modi.esdm.go.id/portal/detailPerusahaan/%d';
+	break;
+default:
+	$data['urlf'] = '';
+	break;
+}
 
-<div style="display: flex; flex-flow: column; height: 100%; margin: 0px; padding: 0px;">
-
-<form action="" method="GET">
-
-	<div style="text-align: center">
-	<div style="float: left;">
-
-	<button name="jump" value="<?php echo $data['id'] - 1; ?>"><?php echo $data['id'] - 1; ?></button>
-
-	</div>
-
-	<label for="website">Website:</label>
-	<input type="text" name="website" value="<?php echo $data['website']; ?>" />
-
-	<label for="type">Type:</label>
-	<input type="text" name="type" value="<?php echo $data['type']; ?>" />
-
-	<label for="id">ID:</label>
-	<input type="number" name="id" value="<?php echo $data['id']; ?>" />
-
-	<input type="submit" hidden />
-	<button type="submit">GO</button>
-
-	<div style="float: right;">
-
-	<button name="jump" value="<?php echo $data['id'] + 1; ?>"><?php echo $data['id'] + 1; ?></button>
-
-	</div>
-	</div>
-
-</form>
-
-<div>AAA</div>
-
-<div style="flex: 1; overflow: auto;">
-<div style="display: flex; height: 100%;">
-
-<?php
+$data['url'] = sprintf($data['urlf'], $data['id']);
 
 $dbconn = pg_connect('dbname=refilter user=postgres password=1234');
 
-$stmt = bin2hex(random_bytes(16));
-pg_prepare($dbconn, $stmt, '
+$stmt1 = bin2hex(random_bytes(16));
+pg_prepare($dbconn, $stmt1, '
 		select
 			auto_id, content
 		from
@@ -92,43 +68,154 @@ pg_prepare($dbconn, $stmt, '
 			auto_id desc
 ');
 
-$res = pg_execute($dbconn, $stmt, [$data['website'], $data['type'], $data['id']]);
-
-function clean($src, $data) {
-	switch([$data['website'], $data['type']]) {
-	case ['modi', null]:
-		$src = preg_replace('/<script.*?\/script>/s', '', $src);
-		$src = preg_replace('/<link.*?\/>/s', '', $src);
-		$src = preg_replace('/<div.*?class="loading.*?\/div>/s', '', $src);
-		$src = preg_replace('/<img.*?\/>/s', '', $src);
-		break;
-	}
-	return $src;
-}
-
+$res = pg_execute($dbconn, $stmt1, [$data['website'], $data['type'], $data['id']]);
 if ($res === false)
-	echo '<p>Not found.</p>';
+	$res = null;
 else {
 	$res = pg_fetch_all($res);
 	if (count($res) == 0)
-		echo '<p>Not found.</p>';
+		$res = null;
 	else
-		printf('
-				<div style="flex: 1;">
-					<iframe style="width: 99%%; height: 99%%;" srcdoc="%s"></iframe>
-				</div>
-				<div style="flex: 1; overflow-y: auto">
-					<div style="font-family: Consolas; white-space: pre-wrap;">%s</div>
-				</div>
-			',
-			htmlspecialchars(clean($res[0]['content'], $data)),
-			htmlspecialchars($res[0]['content'])
-		);
+		$res = $res[0]['content'];
+}
+
+$content = $res;
+$clean = $res;
+
+if ($res != null)
+	switch([$data['website'], $data['type']]) {
+	case ['modi', null]:
+		$clean = preg_replace('/<script.*?\/script>/s', '', $clean);
+		$clean = preg_replace('/<link.*?\/>/s', '', $clean);
+		$clean = preg_replace('/<div.*?class="loading.*?\/div>/s', '', $clean);
+		$clean = preg_replace('/<img.*?\/>/s', '', $clean);
+		break;
+	}
+
+$stmt2 = bin2hex(random_bytes(16));
+pg_prepare($dbconn, $stmt2, '
+	select distinct
+		id
+	from
+		raw
+	where
+		website = $1 and
+		(type = $2 or (type is null and $2 is null))
+	order by
+		id asc
+');
+
+$res = pg_execute($dbconn, $stmt2, [$data['website'], $data['type']]);
+if ($res === false)
+	$res = null;
+else {
+	$res = pg_fetch_all($res);
+	if (count($res) == 0)
+		$res = null;
+	else
+		$res[0];
+}
+
+if ($res == null) {
+	$lb = null;
+	$ub = null;
+	$entries = 0;
+	$holes = 0;
+} else {
+	$lb = $res[0]['id'];
+	$ub = $res[array_key_last($res)]['id'];
+	$entries = count($res);
+	$holes = $ub - $lb + 1 - $entries;
 }
 
 ?>
 
-</div>
+<div style="height: 100vh; display: flex; flex-direction: column; padding: 1rem; box-sizing: border-box;">
+	<div style="flex: 0;">
+		<div style="display: flex;">
+			<div style="flex: 0;">
+				<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="GET" style="display: inline-block; margin: 0;">
+					<input type="hidden" name="website" value="<?php echo $data['website']; ?>" />
+					<input type="hidden" name="type" value="<?php echo $data['type']; ?>" />
+					<input type="hidden" name="id" value="<?php echo $data['id'] - 1; ?>" />
+					<button type="submit"><?php echo $data['id'] - 1; ?></button>
+				</form>
+			</div>
+			<div style="flex: 1; text-align: center;">
+				<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="GET" style="display: inline-block; margin: 0;">
+					<label for="website">Website:</label>
+					<input type="text" name="website" value="<?php echo $data['website']; ?>" />
+					<label for="type">Type:</label>
+					<input type="text" name="type" value="<?php echo $data['type']; ?>" />
+					<label for="id">ID:</label>
+					<input type="number" name="id" value="<?php echo $data['id']; ?>" />
+					<button type="submit">Find</button>
+				</form>
+			</div>
+			<div style="flex: 0;">
+				<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="GET" style="display: inline-block; margin: 0;">
+					<input type="hidden" name="website" value="<?php echo $data['website']; ?>" />
+					<input type="hidden" name="type" value="<?php echo $data['type']; ?>" />
+					<input type="hidden" name="id" value="<?php echo $data['id'] + 1; ?>" />
+					<button type="submit"><?php echo $data['id'] + 1; ?></button>
+				</form>
+			</div>
+		</div>
+	</div>
+	<div style="flex: 0;">
+		<div style="text-align: center;">
+<?php if ($lb != null && $ub != null): ?>
+		<p>
+				lb: <?php echo $lb; ?>
+				&emsp;
+				ub: <?php echo $ub; ?>
+				&emsp;
+				entries: <?php echo $entries; ?>
+				&emsp;
+				holes: <?php echo $holes; ?>
+		</p>
+<?php else: ?>
+		<p>Not found</p>
+<?php endif; ?>
+		</div>
+	</div>
+	<div style="flex: 1; overflow: auto;">
+		<div style="height: 100%; display: flex;">
+			<div style="flex: 1; overflow: auto;">
+<?php if ($lb != null && $ub != null): ?>
+<?php if ($content == null): ?>
+				<div style="width: 100%; height: 100%; display: table; text-align: center;">
+					<span style="display: table-cell; vertical-align: middle;">
+						<?php printf('<a href="%s">%s</a>', $data['url'], $data['url']); ?>
+					</span>
+				</div>
+<?php else: ?>
+				<div style="width: 100%; height: 100%; padding: 1rem; box-sizing: border-box;">
+					<iframe style="width: 100%; height: 100%;" srcdoc="<?php echo htmlspecialchars($clean) ?>"></iframe>
+				</div>
+<?php endif; ?>
+<?php endif; ?>
+			</div>
+			<div style="flex: 1; overflow: auto;">
+<?php if ($lb != null && $ub != null): ?>
+<?php if ($content == null): ?>
+				<div style="width: 100%; height: 100%; display: table; text-align: center;">
+					<span style="display: table-cell; vertical-align: middle;">Not found.</span>
+				</div>
+<?php else: ?>
+				<div style="width: 100%; height: 100%; overflow: auto;">
+					<div style="white-space: pre; font-family: Consolas;"><?php echo htmlspecialchars($content); ?></div>
+				</div>
+<?php endif; ?>
+<?php endif; ?>
+			</div>
+		</div>
+	</div>
 </div>
 
-</div>
+<style>
+	body {
+		padding: 0;
+		margin: 0;
+	}
+</style>
